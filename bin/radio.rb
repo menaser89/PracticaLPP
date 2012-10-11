1beebfd87933f2f6d428d8d8110791d3afc045ed
menaser89 <menaser89@gmail.com>
2012-10-11 11:35:34 +0100

    Primer commit

---
+++ bin/radio.rb
@@ -0,0 +1,17 @@
+# File:  radio.rb
+ 
+class Radio
+ 
+  def initialize( num )
+    raise ArgumentError, 'El argumento no es numerico' unless num.is_a? Numeric
+    raise ArgumentError, 'El argumento no es un numero positivo' unless (num > 0)
+#     raise ArgumentError, 'El argumento es numerico negativo' unless x.is_a? Numeric
+#     raise unless num.is_a?(negativo)
+    @x = num
+  end
+ 
+  def calculoradio(  )
+    ((@x)/(2*(3.14)))
+  end
+ 
+end
